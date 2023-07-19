7ae1fbdb0401e9d556ba0ae331ecfe8dd900f00f
Vadim21school <vadikburduzha@yandex.ru>
2023-07-19 17:52:39 +0300

    secondCommit

--- Assets/Scripts/SpawnEnemies.cs
+++ Assets/Scripts/SpawnEnemies.cs
@@ -3,32 +3,20 @@ using UnityEngine;
 
 public class SpawnEnemies : MonoBehaviour
 {
-    [SerializeField] private GameObject enemy;
-    private Vector3[] postitionsEnemies;
-    // Start is called before the first frame update
+    [SerializeField] private Player _template;
 
     private void Start()
     {
         StartCoroutine(SpawnEnemy());
     }
-    // Update is called once per frame
-
+   
     private IEnumerator SpawnEnemy()
     {
-        postitionsEnemies = new Vector3[]
-        {
-            new Vector3(-8, 2.67f, 0),
-            new Vector3(-0.09f, -0.4f, 0),
-            new Vector3(7.86f, 2.59f, 0),
-            new Vector3(6.91f, -3.4f, 0),
-            new Vector3(-8, -3.4f, 0)
-        };
-
         var waitForTwoSeconds = new WaitForSeconds(2f);
 
-        for (int i = 0; i < postitionsEnemies.Length; i++)
+        for (int i = 0; i < transform.childCount; i++)
         {
-            Instantiate(enemy, postitionsEnemies[i], Quaternion.identity);
+            Instantiate(_template, transform.GetChild(i).position, Quaternion.identity);
             yield return waitForTwoSeconds;
         }
     }
