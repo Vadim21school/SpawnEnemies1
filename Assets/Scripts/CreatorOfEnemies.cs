7e0f5ca48086ea8acdcf6b09a002b4a09bfaeeae
Vadim21school <vadikburduzha@yandex.ru>
2023-07-19 21:40:10 +0300

    thirdCommit

--- Assets/Scripts/CreatorOfEnemies.cs
+++ Assets/Scripts/CreatorOfEnemies.cs
@@ -1,7 +1,7 @@
 using System.Collections;
 using UnityEngine;
 
-public class SpawnEnemies : MonoBehaviour
+public class CreatorOfEnemies : MonoBehaviour
 {
     [SerializeField] private Player _template;
 
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,17 +1,9 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    void Start()
+    private void Start()
     {
-        
-    }
-
-
-    void Update()
-    {
-        
+        Debug.Log("I was born");
     }
 }
